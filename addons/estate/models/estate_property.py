cff5a0db19c6bc091c20c7402a9346df560a0a08
Hudson-59 <hudsonamadeuss@gmail.com>
2025-09-04 19:25:23 +0700

    update views xml and menus

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -5,14 +5,14 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
 
-    name = fields.Char(required=True)
+    name = fields.Char('Title', required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(copy=False, default=lambda self: fields.Datetime.now() + relativedelta(months=3))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
@@ -23,8 +23,7 @@ class EstateProperty(models.Model):
             ('south', 'South'),
             ('east', 'East'),
             ('west', 'West'),
-        ]
-    )
+        ])
 
     active = fields.Boolean(default=False)
 
@@ -35,8 +34,8 @@ class EstateProperty(models.Model):
             ('offer_accepted', 'Offer Accepted'),
             ('sold', 'Sold'),
             ('canceled', 'Canceled'),
-        ]
+        ],
         required=True,
-        copy=False,
+        copy=True,
         default='new',
     )
